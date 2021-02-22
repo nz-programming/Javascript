91330b4ea559b0cb2a9e7d7b8465d1039c2d516e
Hiroki <s.s.ka.tu.ok@gmail.com>
2021-02-22 11:32:20 +0900

    不要なconsole.log()を削除

--- main.js
+++ main.js
@@ -7,22 +7,14 @@ const inputnum1 = document.getElementById('num1');
 const inputnum2 = document.getElementById('num2');
 
 function getNum1(){
-  console.log(inputnum1.value);
-  console.log(typeof inputnum1.value);
   let realnum1;
   realnum1 = Number.parseFloat(inputnum1.value);
-  console.log(realnum1);
-  console.log(typeof realnum1);  
   return realnum1;
 }
 
 function getNum2(){
-  console.log(inputnum2.value);
-  console.log(typeof inputnum2.value);
   let realnum2;
   realnum2 = Number.parseFloat(inputnum2.value);
-  console.log(realnum2);
-  console.log(typeof realnum2);
   return realnum2;
 }
 
@@ -31,34 +23,26 @@ function showresult(result){
 }
 
 function sum(){
-  console.log(getNum1() + getNum2());
   let sum;
   sum = getNum1() + getNum2();
-  console.log(sum);
   showresult(sum);
 }
 
 function sub(){
-  console.log(getNum1() - getNum2());
   let sub;
   sub = getNum1() - getNum2();
-  console.log(sub);
   showresult(sub);
 }
 
 function mul(){
-  console.log(getNum1() * getNum2());
   let mul;
   mul = getNum1() * getNum2();
-  console.log(mul);
   showresult(mul);
 }
 
 function div(){
-  console.log(getNum1() / getNum2());
   let div;
   div = getNum1() / getNum2();
-  console.log(div);
   showresult(div);
 }
 
